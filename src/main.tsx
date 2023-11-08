ea5d8cd593d02b967299539379bd82d37eaa72d7
Andy <carcamo@andrews.edu>
2023-11-07 18:30:55 -0600

    wrapped app in themeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { CssBaseline } from '@mui/material'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CssBaseline/>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
