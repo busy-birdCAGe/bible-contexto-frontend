a3289b45a838d5b2bf9ef8d553d45661589ae2c4
Andy <carcamo@andrews.edu>
2023-11-08 23:34:22 -0500

    created title component

--- src/assets/pages/GamePage.tsx
+++ src/assets/pages/GamePage.tsx
@@ -2,22 +2,23 @@ import Box from "@mui/material/Box/Box";
 import SectionHeader from "../../components/SectionHeader";
 import Guesses from "../../components/Guesses";
 import mockGuessData from "../../mockGuessData.json";
-
+import Title from "../../components/Title";
 
 const GamePage = () => {
   const current = [
     {
       number: 4000,
-      word: "well"
+      word: "well",
     },
   ];
 
   return (
     <Box>
+      <Title title="Bible Contexto" />
       <SectionHeader title="current" />
       <Guesses guesses={current} />
       <SectionHeader title="previous" />
-      <Guesses guesses={mockGuessData}/>
+      <Guesses guesses={mockGuessData} />
     </Box>
   );
 };
---
+++ src/components/Title.tsx
@@ -0,0 +1,24 @@
+import Box from "@mui/material/Box/Box";
+
+interface TitleProps {
+  title: string;
+}
+const Title = ({ title }: TitleProps) => {
+  return (
+    <Box
+      sx={{
+        color: "white",
+        textAlign: "center",
+        ml: "20px",
+        fontFamily: "monospace",
+        fontSize: 32,
+        fontWeight: "700",
+        letterSpacing: ".1px",
+      }}
+    >
+      {title}
+    </Box>
+  );
+};
+
+export default Title;
