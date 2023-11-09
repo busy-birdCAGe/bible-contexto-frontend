4c25502d1dd9b7b519f0e74197e98342139bd960
Andy <carcamo@andrews.edu>
2023-11-08 22:36:31 -0500

    color on WordCard updates based on guess

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -6,21 +6,33 @@ interface WordCardProps {
 }
 
 const WordCard = ({ number, word }: WordCardProps) => {
+  let backgroundImage: string;
+
+  if (number <= 300) {
+    backgroundImage =
+      "linear-gradient(to right, rgba(5, 255, 0, 1) 5%, rgba(5, 255, 0, 0.2) 5% 50%, rgba(0, 0, 0, 0) 50%)";
+  } else if (number <= 1000) {
+    backgroundImage =
+      "linear-gradient(to right, rgba(255, 245, 0, 1) 5%, rgba(255, 245, 0, 0.2) 5% 50%, rgba(0, 0, 0, 0) 50%)";
+  } else {
+    backgroundImage =
+      "linear-gradient(to right, rgba(255, 0, 0, 1) 5%, rgba(255, 0, 0, 0.2) 5% 50%, rgba(0, 0, 0, 0) 50%)";
+  }
+
   return (
     <Box
       sx={{
         width: "355px",
         height: "70px",
         bgcolor: "#FFFFFF",
-        backgroundImage:
-          "linear-gradient(to right, rgba(5, 255, 0, 1) 5%, rgba(5, 255, 0, 0.2) 5% 50%, rgba(0, 0, 0, 0) 50%)",
+        backgroundImage: backgroundImage,
         borderRadius: "20px",
         overflow: "hidden",
         display: "flex",
         alignItems: "center",
         fontSize: 24,
         fontFamily: "monospace",
-        mt: "6px"
+        mt: "6px",
       }}
     >
       <Box
@@ -29,7 +41,7 @@ const WordCard = ({ number, word }: WordCardProps) => {
           width: "235px",
           justifyContent: "left",
           ml: "40px",
-          fontWeight:"700"
+          fontWeight: "700",
         }}
       >
         {word}
