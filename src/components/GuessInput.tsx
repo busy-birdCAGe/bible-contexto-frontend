290fd76c8b155cc864d072ea814c4f248cf6915b
Andy <carcamo@andrews.edu>
2023-11-09 21:59:32 -0500

    wip turned GuessInput into form functionality

--- src/components/GuessInput.tsx
+++ src/components/GuessInput.tsx
@@ -1,26 +1,41 @@
+import Box from "@mui/material/Box/Box";
 import Input from "@mui/material/Input";
+import { useState } from "react";
 
-interface GuessInputProps {
-  guess?: string;
-}
+const GuessInput = () => {
+  const [inputValue, setInputValue] = useState("");
 
-const GuessInput = ({ guess }: GuessInputProps) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Guess was:", inputValue);
+
+    // Add your further submission logic here
+    setInputValue("");
+  };
+
   return (
-    <Input
-    disableUnderline={true}
-    placeholder={"Enter a word"}
-      sx={{
-        pl:"10px",
-        width: "355px",
-        height: "50px",
-        // color: "white",
-        bgcolor: "rgba(217, 217, 217, 1)",
-        borderRadius: "10px",
-        fontFamily: "monospace",
-        fontSize: 24,
-        fontWeight: "700",
-      }}
-    />
+    <Box component="form" onSubmit={handleSubmit}>
+      <Input
+        disableUnderline={true}
+        placeholder={"Enter a word"}
+        value={inputValue}
+        onChange={handleChange}
+        sx={{
+          pl: "10px",
+          width: "355px",
+          height: "50px",
+          bgcolor: "rgba(217, 217, 217, 1)",
+          borderRadius: "10px",
+          fontFamily: "monospace",
+          fontSize: 24,
+          fontWeight: "700",
+        }}
+      />
+    </Box>
   );
 };
 
