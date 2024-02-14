b1ddf2f9d23b64785975c2b0596904c159df1e29
Carson O'Ffill <carson@adra.cloud>
2024-02-14 16:07:09 -0500

    improve help text

--- src/components/HelpSection.tsx
+++ src/components/HelpSection.tsx
@@ -27,23 +27,38 @@ const HelpSection = ({ setVisibility, visible }: HelpSectionProps) => {
         width: "355px",
         fontFamily: "monospace",
         fontWeight: "500",
-        fontSize: 18
+        fontSize: 18,
       }}
     >
-      <Box sx={{marginLeft: "auto", pr: "10px", pt: "10px", lineHeight: ".5"}}
->
+      <Box
+        sx={{ marginLeft: "auto", pr: "10px", pt: "10px", lineHeight: ".5" }}
+      >
         <IoClose
           onClick={hideHelp}
-          style={{ color: "white", fontSize: "1.5em", marginRight: "auto", width: "100%"}}
+          style={{
+            color: "white",
+            fontSize: "1.5em",
+            marginRight: "auto",
+            width: "100%",
+          }}
         />
       </Box>
-      <Box sx = {{px: "15px", pb:"10px"}}>
+      <Box sx={{ px: "15px", pb: "10px" }}>
         <Title title="How To Play:" size={24} />
-        <Box sx={{ color: "white", textAlign: "left", my: "8px"  }}>Every day a random word is chosen out of the NIV bible, and your goal is to guess it.</Box>
-        <Box sx={{ color: "white", textAlign: "left"  }}>Each of your guesses will be ranked against the word of the day by how often the word appears in similar contexts.</Box>
+        <Box sx={{ color: "white", textAlign: "left", my: "8px" }}>
+          Every day a random word is chosen out of the NIV bible, and your goal
+          is to guess it.
+        </Box>
+        <Box sx={{ color: "white", textAlign: "left", my: "8px" }}>
+          Each of your guesses will be scored against the word of the day by how
+          often the word appears in similar contexts.
+        </Box>
+        <Box sx={{ color: "white", textAlign: "left" }}>
+          Words with lower scores are closer to the word of the day, which has a
+          score of 1.
+        </Box>
       </Box>
-    
-      </Box>
+    </Box>
   );
 };
 
