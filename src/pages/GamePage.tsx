c529e23d8957862d5d4be0561c6493a9e398f219
Carson O'Ffill <69762440+karzunn@users.noreply.github.com>
2025-01-16 14:38:39 -0500

    Add unit testing (#37)
    * add basic test
    
    * add test utils
    
    * update node
    
    * update node in all envs
    
    * add 3 e2e tests
    
    * simplify tests with more utils
    
    * add test case for shared game
    
    * remove log
    
    * unit tests to single file
    
    * remove unnecessary folder
    
    ---------
    
    Co-authored-by: Carson O'Ffill <karzunn@users.noreply.github.com>

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -16,7 +16,7 @@ import {
   getPathToken,
   decodeGameToken,
   getGameName,
-  getColorCounts,
+  getColorCounts
 } from "../utils";
 import { State } from "../GameState";
 import ErrorMessage from "../components/ErrorMessage";
@@ -59,7 +59,6 @@ const GamePage = () => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
-
 
   const handleGuess = () => {
     const trimmedInput = inputValue.trim();
